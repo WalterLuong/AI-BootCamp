90f9520fe34c5a715e8560e23a0a43f882a494e6
WalterLuong <wluong@student.42.fr>
2023-03-09 23:23:36 +0100

    maj

--- PYTHON01/ex02/vector.py
+++ PYTHON01/ex02/vector.py
@@ -16,16 +16,42 @@ def is_row(values) -> bool :
 		return True
 	return False
 
+def is_size(values) -> bool :
+	"""Check if the vector is initialized by a size"""
+	if isinstance(values, int):
+		return True
+	return False
+
+def is_range(values) -> bool:
+	"""Check if the vector is initialized by a range"""
+	if isinstance(values, tuple) and len(values) == 2:
+		if all(isinstance(x, int) for x in values):
+			return True
+	return False
+
 class Vector:
 	"""A vector class which can be used for maths operations"""
 	def __init__(self, values) -> None:
 		"""Initialization of the Vector"""
-		if is_row(values):
+		if is_size(values):
+			self.values = []
+			for x in range(values):
+				self.values.append([float(x)])
+			self.shape = (values, 1)
+		elif is_row(values):
 			self.values = values
 			self.shape = (1, len(values[0]))
 		elif is_column(values):
 			self.values = values
 			self.shape = (len(values), 1)
+		elif is_range(values):
+			if values[0] > values[1]:
+				raise ValueError("Can't initialize a Vector with a range (a, b) where a > b")
+			else:
+				self.values = []
+				for x in range(values[0], values[1]):
+					self.values.append([float(x)])
+				self.shape = (values[1] - values[0], 1)
 		else:
 			raise ValueError("\033[1;31mBad parameters for the vector's creation\033[m")
 		
